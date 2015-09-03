b29dd1881784cd8d940ce09edb001b55f22faed6
M.D. van Es <es@mdworld.nl>
2015-09-03 07:41:03 +0200

    added travis config, fixes for newlines

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,9 +6,10 @@
  * Licensed under the MIT license.
  */
 
-'use strict';
-
 module.exports = function (grunt) {
+
+    'use strict';
+    require('load-grunt-tasks')(grunt);
 
     // Project configuration.
     grunt.initConfig({
@@ -56,11 +57,6 @@ module.exports = function (grunt) {
 
     // Actually load this plugin's task(s).
     grunt.loadTasks('tasks');
-
-    // These plugins provide necessary tasks.
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
     // Whenever the "test" task is run, first clean the "tmp" dir, then run this
     // plugin's task(s), then test the result.
